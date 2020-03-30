feea832dfbb845ceb83d3a7de5e11fa1b774b5bc
kawamichal <michal.kawa@hotmail.com>
2020-03-30 17:18:40 +0200

    Countdown added

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 // Play button logic
+
 var playbtn = document.querySelector(".playbtn");
 
 playbtn.addEventListener("click", playBtnClick);
 
-function playBtnClick () {
-    playbtn.classList.add("hidden")
+function playBtnClick() {
+  if (playbtn.innerHTML === "PLAY") {
+    playbtn.innerHTML = "PAUSE";
+    resumeCountdown();
+  } else {
+    playbtn.innerHTML = "PLAY";
+  }
+}
+
+// Countdown
+var seconds = document.getElementById("countdown").textContent;
+
+function resumeCountdown() {
+  var countdown = setInterval(function() {
+    seconds--;
+    document.getElementById("countdown").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+  }, 1000);
 }
