40b2a876c38c27a5907e7fcdf01b317b2e50393b
kawamichal <michal.kawa@hotmail.com>
2020-04-01 11:09:48 +0200

    Logic finished

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-// Variables
+// Word factory
+class WordCreator {
+  constructor(
+    word,
+    forbidden1,
+    forbidden2,
+    forbidden3,
+    forbidden4,
+    forbidden5
+  ) {
+    this.word = word;
+    this.forbidden = [
+      forbidden1,
+      forbidden2,
+      forbidden3,
+      forbidden4,
+      forbidden5
+    ];
+  }
+}
 
+// Selectors
 var playBtn = document.querySelector(".playbtn");
 var resetBtn = document.querySelector(".resetbtn");
 var countdownID = document.getElementById("countdown");
-var pause = true;
-var seconds = 90;
+var correctBtn = document.getElementById("correct");
+var wrongBtn = document.getElementById("wrong");
+var yellowScore = document.getElementById("yellow-score");
+var greenScore = document.getElementById("green-score");
+var turnNotification = document.querySelector(".turn-notification");
 
+//Flags
+var pause = true; // Set the timer to paused by default
+var turn = true; // True - yellow team, False - green team
+
+//Values
+var gameTime = 3; //
+var seconds = gameTime;
+var yellow = 0;
+var green = 0;
+
+var wordList = [];
 
 // Function calls
-
 playBtn.addEventListener("click", playBtnClick);
 resetBtn.addEventListener("click", resetBtnClick);
+correctBtn.addEventListener("click", correctBtnClick);
+wrongBtn.addEventListener("click", wrongBtnClick);
 
+wordsDb();
 playCountdown();
 
-// Play button logic
+// Functions
 
+// Play button logic
 function playBtnClick() {
   if (playBtn.innerHTML === "PLAY") {
     playBtn.innerHTML = "PAUSE";
@@ -26,15 +63,15 @@ function playBtnClick() {
   }
 }
 
+// Reset button logic
 function resetBtnClick() {
   playBtn.innerHTML = "PLAY";
   pause = true;
-  seconds = 90;
-  countdownID.textContent = "90 seconds";
+  seconds = gameTime;
+  countdownID.textContent = seconds + " seconds";
 }
 
-// Countdown
-
+// Countdown logic
 function playCountdown() {
   clearInterval(countdown);
   var countdown = setInterval(function() {
@@ -46,15 +83,96 @@ function playCountdown() {
       }
       seconds--;
     }
-
     if (seconds < 0) {
       clearInterval(countdown);
       countdownID.textContent = "Time is up!";
       setTimeout(function() {
-        countdownID.textContent = "90 seconds";
-      }, 2500);
-      playBtnClick();
-      playCountdown();
+        resetBtnClick();
+        if (turn) {
+          turn = false;
+        } else {
+          turn = true;
+        }
+        backgroundSwitch();
+        playCountdown();
+      }, 2000);
     }
   }, 1000);
+}
+
+// Background switch
+function backgroundSwitch() {
+  if (turn) {
+    document.body.style.backgroundColor = "#feffa3";
+    turnNotification.innerText = "Yellow team's turn!";
+  } else {
+    document.body.style.backgroundColor = "#a7f69a";
+    turnNotification.innerText = "Green team's turn!";
+  }
+}
+
+// Score counters
+function correctCounter() {
+  if (turn) {
+    yellow += 1;
+    yellowScore.innerText = yellow;
+  } else {
+    green += 1;
+    greenScore.innerText = green;
+  }
+}
+
+function wrongCounter() {
+  if (turn) {
+    yellow -= 1;
+    yellowScore.innerText = yellow;
+  } else {
+    green -= 1;
+    greenScore.innerText = green;
+  }
+}
+
+// Correct button logic
+function correctBtnClick() {
+  if (!pause) {
+    correctCounter();
+    wordRandomizer();
+  }
+}
+
+// Wrong button logic
+function wrongBtnClick() {
+  if (!pause) {
+    wrongCounter();
+    wordRandomizer();
+  }
+}
+
+// Word randomizer
+function wordRandomizer() {
+  // Generating a random number
+  var randomNumber = Math.floor(Math.random() * wordList.length);
+
+  // Placing word
+  document.querySelector(".main-word").innerText = wordList[randomNumber].word;
+  for (var i = 0; i < 5; i++) {
+    document.querySelectorAll(".frb-word")[i].innerText =
+      wordList[randomNumber].forbidden[i];
+  }
+}
+
+// Declared words
+function wordsDb() {
+  const word1 = new WordCreator("1A", "A", "A", "A", "A", "A");
+  wordList.push(word1);
+  const word2 = new WordCreator("2B", "B", "B", "B", "B", "B");
+  wordList.push(word2);
+  const word3 = new WordCreator("3C", "C", "C", "C", "C", "C");
+  wordList.push(word3);
+  const word4 = new WordCreator("4D", "D", "D", "D", "D", "D");
+  wordList.push(word4);
+  const word5 = new WordCreator("5E", "E", "E", "E", "E", "E");
+  wordList.push(word5);
+  const word6 = new WordCreator("6F", "F", "F", "F", "F", "F");
+  wordList.push(word6);
 }
